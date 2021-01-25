dd3c3e1a8e898a3cf207268553217a868454e99c
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-01-25 11:57:22 -0300

    agrega gsap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,16 @@
 import './Header.css'
+import logo from '../../assets/logo.svg'
 
 const Header = () => {
   return (
     <div className="Header">
-      <div>cero</div>
-      <nav>
+      <img className="Header__logo" src={logo} alt="Logo de Cero" />
+      <nav className="Header__navegacion">
         <a className="Header__link" href="#hola">Nosotros</a>
         <a className="Header__link" href="#hola">Trabajemos juntos</a>
         <a className="Header__link" href="#hola">Contacto</a>
       </nav>
-      <button>Empieza ya</button>
+      <button className="Header__cta">Empieza ya</button>
     </div>
   )
 }
--- src/components/Superior/Superior.js
+++ src/components/Superior/Superior.js
@@ -1,9 +1,25 @@
 import './Superior.css'
+import { gsap } from 'gsap'
+import { useEffect, useRef } from 'react'
 
 const Superior = () => {
+
+  const header = useRef()
+
+  useEffect(() => {
+    gsap.to(header.current, {
+      scrollTrigger: {
+        trigger: header.current,
+        scrub: 1
+      },
+      y: -100,
+      duration: 2
+    })
+  }, [header])
+
   return (
     <div className="Superior">
-      <p className="Superior__mensaje_principal">Mejora tu comunicación sumando cero</p>
+      <p className="Superior__mensaje_principal" ref={header}>Mejora tu comunicación sumando cero</p>
     </div>
   )
 }
