260b44d1596048b5c3abeddc9834c9f16b715ed3
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-04-20 11:31:27 -0400

    prueba para fijar tamaño de viewport

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,32 +21,30 @@ const Footer = () => {
             <p>Botlab, Inc.</p>
           </div>
         </div>
-        <div className="Footer__informacion">
-          <div className="Footer__links">
-            <div>
-              <p>Apoyado por</p>
-              <p>
-                <a target="_blank" rel="noreferrer noopener" href="https://www.startupchile.org/es/home-es/">
-                  <img className="Footer_logo_sup_chile" src={logoSUPChile} alt="Logo Start-up Chile" />
-                </a>
-              </p>
-            </div>
-            <div><Link to="/contacto">Contacto</Link></div>
-            <div><a target="_blank" rel="noreferrer noopener" href="https://status.cero.ai/">STATUS</a></div>
+        <div className="Footer__links">
+          <div>
+            <p>Apoyado por</p>
+            <p>
+              <a target="_blank" rel="noreferrer noopener" href="https://www.startupchile.org/es/home-es/">
+                <img className="Footer_logo_sup_chile" src={logoSUPChile} alt="Logo Start-up Chile" />
+              </a>
+            </p>
           </div>
-          <div className="Footer__integraciones">
-            <p>Integraciones</p>
-            <div className="Footer__contenedor_integraciones">
-              <a target="_blank" rel="noreferrer noopener" href="https://www.softwaredentalink.com/">
-                <img className="Footer__logo_integracion" src={logoDentalink} alt="Logo Dentalink" />
-              </a>
-              <a target="_blank" rel="noreferrer noopener" href="https://www.siliconriver.cl/koplandsymphony/">
-                <img className="Footer__logo_integracion" src={logoKopland} alt="Logo Kopland" />
-              </a>
-              <a target="_blank" rel="noreferrer noopener" href="https://www.sked24.com/es/citas-medicas/">
-                <img className="Footer__logo_integracion" src={logoSked} alt="Logo Sked" />
-              </a>
-            </div>
+          <div><Link to="/contacto">Contacto</Link></div>
+          <div><a target="_blank" rel="noreferrer noopener" href="https://status.cero.ai/">STATUS</a></div>
+        </div>
+        <div className="Footer__integraciones">
+          <p>Integraciones</p>
+          <div className="Footer__contenedor_integraciones">
+            <a target="_blank" rel="noreferrer noopener" href="https://www.softwaredentalink.com/">
+              <img className="Footer__logo_integracion" src={logoDentalink} alt="Logo Dentalink" />
+            </a>
+            <a target="_blank" rel="noreferrer noopener" href="https://www.siliconriver.cl/koplandsymphony/">
+              <img className="Footer__logo_integracion" src={logoKopland} alt="Logo Kopland" />
+            </a>
+            <a target="_blank" rel="noreferrer noopener" href="https://www.sked24.com/es/citas-medicas/">
+              <img className="Footer__logo_integracion" src={logoSked} alt="Logo Sked" />
+            </a>
           </div>
         </div>
       </div>
