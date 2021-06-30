4f8e4525d14634491a76cb56e72f27a53013770e
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-06-30 12:01:57 -0400

    alinea mas o menos textos

--- src/components/AcercaDe/AcercaDe.js
+++ src/components/AcercaDe/AcercaDe.js
@@ -35,13 +35,13 @@ const AcercaDe = () => {
     <div className="AcercaDe">
       <h1 className="AcercaDe__mensaje_principal" ref={header}>Sobre Cero</h1>
       <p className="AcercaDe__bajada" ref={p1}>
-        Estamos en una misión para construir mejor comunicación entre servicios de salud y pacientes. Nos ponemos en el lugar de los pacientes (¡también lo somos!) para construir servicios que se adapten a su día a día, entregando información y ayudándoles a completar tareas importantes de una forma natural, cómoda y eficiente para tod@s.
+        Estamos en una misión para construir mejor comunicación entre servicios de salud y pacientes. Nos ponemos en el lugar de los pacientes (¡también lo somos!) para construir servicios que se adapten a su día a día, entregando información y ayudándoles a completar tareas importantes de forma natural, cómoda y eficiente para tod@s.
       </p>
       <p className="AcercaDe__bajada">
-        En Cero creemos que una buena comunicación requiere ir un paso más allá de solo entregar un mensaje de texto. Creemos en generar una sintonía entre dos partes que incluya atención, positividad y coordinación mutua. Aplicamos las últimas técnicas de Inteligencia Artificial desarrollada por nuestro propio equipo (en específico "Aprendizaje Profundo"
+        En Cero creemos que una buena comunicación requiere ir un paso más allá de solo entregar un mensaje de texto; creemos en generar una sintonía entre dos partes que incluya atención, positividad y coordinación mutua. Aplicamos las últimas técnicas de Inteligencia Artificial desarrolladas por nuestro propio equipo (en específico, "Aprendizaje
       </p>
       <p className="AcercaDe__bajada">
-        basado en "Atención Neuronal", pero la verdad es que ¿a quién le importa? 😉) para comprender a las personas y generar un nuevo estándar de comunicación automatizada que permita a organizaciones usar de manera más eficiente sus recursos en pos de entregar un servicio cada vez mejor. Mejor comunicación sumando CERO.
+        Profundo" basado en "Atención Neuronal", pero la verdad es que ¿a quién le importa? 😉) para comprender a las personas y generar un nuevo estándar de comunicación automatizada que permita a organizaciones usar sus recursos de manera más eficiente, en pos de entregar un servicio cada vez mejor. Mejor comunicación sumando CERO.
       </p>
     </div>
   )
